af5381ab8c910787cd3696fb99c02d8ec6b058ac
Travis Hyyppä <travishyyppa@gmail.com>
2018-03-15 14:48:38 -0600

    More comments!

--- include/tcp.h
+++ include/tcp.h
@@ -1,22 +1,64 @@
 #ifndef tcp_h
 #define tcp_h
 
-#include "ide_defs.h"
-#include "user_config.h"
+#include <ide_defs.h>
+#include <user_config.h>
 #include <user_interface.h>
 #include <espconn.h>
-#include "osapi.h"
+#include <osapi.h>
 #include "../user/input_handler.h"
+#include "../user/user_config.h"
 
 typedef void (handler)( char * );
 
+/**
+ * Start the TCP server.
+ * This function continuously checks for a wifi connection
+ * When the connection is successful it triggers the tcpserver_init
+ * method.
+ * @param port
+ */
 void ICACHE_FLASH_ATTR start_tcp_server( uint32 port );
+
+/**
+ * Sets up the TCP server and registers callbacks to handle input.
+ * @param port
+ */
 void ICACHE_FLASH_ATTR tcpserver_init( uint32 port );
+
+/**
+ * Set a callback function that will handle parsing through the input
+ * and executing commands.
+ * @param cb
+ */
 void ICACHE_FLASH_ATTR set_input_handler( handler cb );
-void ICACHE_FLASH_ATTR tcp_send( char *string );
-void ICACHE_FLASH_ATTR tcp_disconnect();
-LOCAL void ICACHE_FLASH_ATTR tcp_server_listen( void *espconn );
+
+/**
+ * Send string to all connected clients
+ * Basically a wrapper for tcp_server_multi_send
+ * Either works
+ * @param message
+ */
+void ICACHE_FLASH_ATTR tcp_send( char *message );
 LOCAL void tcp_server_multi_send( char *message );
+
+/**
+ * Kick all clients from the server and stop server
+ */
+void ICACHE_FLASH_ATTR tcp_disconnect();
+
+/**
+ * Sets a callback for each connected client to handle input
+ * @param espconn
+ */
+LOCAL void ICACHE_FLASH_ATTR tcp_server_listen( void *espconn );
+
+/**
+ * This function is where the TCP input is finally sent off to the input handler
+ * @param arg
+ * @param received
+ * @param length
+ */
 LOCAL void ICACHE_FLASH_ATTR tcp_server_recv_cb( void *arg, char *received, unsigned short length );
 
 #endif //tcp_h
--- user/input_handler.h
+++ user/input_handler.h
@@ -2,28 +2,57 @@
 #define input_handler_h
 
 #include <stdlib.h>
-
-#include "ide_defs.h"
-#include "user_config.h"
-#include "osapi.h"
+#include <user_config.h>
 #include <tcp.h>
 #include <pins.h>
+#include <ide_defs.h>
+#include <osapi.h>
 
+/**
+ * Define a function that will be used to handle input
+ * the "input_handler" function is a fine choice.
+ */
 #define INPUT_HANDLER input_handler
-
 typedef void (handler)( char * );
 
+/**
+ * This is where the magic happens.
+ * In this function is a switch statement
+ * that runs the commands sent over tcp.
+ * @param input
+ */
 void input_handler( char *input );
+void arrow_key( char *input );
 void move_up();
 void move_down();
 void stop();
-void display( char *data );
-void startActionTimeout();
-void displaySerial( uint32 data );
-void overrideDisplay();
-void releaseDisplay();
 void reboot();
 void help();
-void arrow_key( char *input );
+
+/**
+ * Send a three digit number to the 7 segment LCD
+ *
+ * todo: This does not work properly yet!
+ * @param data
+ */
+void display( char *data );
+void displaySerial( uint32 data );
+
+/**
+ * Whenever an action is performed be sure to
+ * cancel it after a period of time. Holding down
+ * the "down" button indefinitely wouldn't be good
+ * at all.
+ */
+void startActionTimeout();
+
+/**
+ * Cut off the LCD signal from the original controller
+ * so that we can inject our own.
+ *
+ * todo: These don't work properly yet!
+ */
+void overrideDisplay();
+void releaseDisplay();
 
 #endif //input_handler_h
